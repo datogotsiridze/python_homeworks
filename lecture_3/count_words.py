bd4ecab5548f63107cb11e2ac2d3944faa4a2561
datogotsiridze <datogotsi@gmail.com>
2026-06-09 00:10:09 +0400

    homework_3

---
+++ lecture_3/count_words.py
@@ -0,0 +1,27 @@
+x = "python is very interesting language"
+n = 0
+if not x:
+    n = 0
+
+for y in x:
+    if y == "t":
+
+      n += 1
+
+print (f"letter t appears {n} times")
+
+
+
+
+
+a = "how many words we have?"
+n = 1
+
+if not a:
+   n = 0
+
+for b in a:
+   if b == " ":
+      n = n +1
+
+print(f"sentence has {n} words")
