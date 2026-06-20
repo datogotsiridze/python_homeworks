079f493707e79a539fdb5f3641887744e03605de
datogotsiridze <datogotsi@gmail.com>
2026-06-20 13:16:19 +0400

    Add files via upload

---
+++ homework_5/exercise_5.py
@@ -0,0 +1,17 @@
+numbers = [3, 8, 11, 4, 7, 10]
+
+evens = []
+
+ 
+
+for i in numbers:
+
+    if i % 2 == 0:
+
+      evens.append(i)
+
+     
+
+print ( f" {numbers}  = full list of numbers " )
+
+print (f"there we have this evens: {evens} ")
